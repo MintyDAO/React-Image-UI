38e6153bedd1d9061fa4798c228fd97fb6975b0c
ahmadch-25 <83905076+ahmadch-25@users.noreply.github.com>
2022-12-28 20:27:19 +0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'minty-ai-art',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
